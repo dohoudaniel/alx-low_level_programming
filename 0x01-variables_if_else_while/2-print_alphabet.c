82c12511b2499a14af541c3021fc1cf580b26892
Dohou Daniel <dohoudaniel@example.com>
2022-09-09 16:42:22 -0700

    Printing the alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * Return: always return 0
+ *
+ * function main - Lists alphabets a to z
+ *
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a' ; a <= 'z' ; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
