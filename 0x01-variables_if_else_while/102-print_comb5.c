7cbd93f8218e4947b0bb7ad240ae65d6342ffaf9
Dohou Daniel <dohoudaniel@example.com>
2022-09-09 19:48:44 -0700

    Time to rest

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * main - two digits
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int d;
+	int e;
+
+	for (d = 0; d <= 98; d++)
+	{
+		for (e = 0; e <= 98; e++)
+		{
+		       putchar((d / 10) + '0');
+		       putchar((d % 10) + '0');
+		       putchar(' ');
+		       putchar((e / 10) + '0');
+		       putchar((e % 10) + '0');
+
+		       if (d == 98 && e ==98)
+			       continue;
+
+		       putchar(',');
+		       putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
