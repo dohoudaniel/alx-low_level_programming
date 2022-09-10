7db10039bb07e6d5c42c4e28794138658690e780
Dohou Daniel <dohoudaniel@example.com>
2022-09-09 18:05:25 -0700

    Printing of numbers in base 10, from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * function main - prints numbers 0 to 9
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 0; d < 10; d++)
+	{
+		printf("%i", d);
+	}
+	printf("\n");
+	return (0);
+}
