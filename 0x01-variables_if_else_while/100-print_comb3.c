3b78077934993685c77b8fbde4e049e5b15a5276
Dohou Daniel <dohoudaniel@example.com>
2022-09-09 18:54:25 -0700

    Advanced task 1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * function main - a program that prints
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int f;
+	int l;
+
+	for (f = 10; f <= 19; f++)
+	{
+		for (l = 10; l <= 19; l++)
+		{
+			if ((l % 10) > (f % 10))
+			{
+				putchar((f % 10) + '0');
+				putchar((l % 10) + '0');
+				if (f != 18 || l != 19)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
