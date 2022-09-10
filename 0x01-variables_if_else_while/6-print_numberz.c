127b51524f7f01094f1ae62f3ca61946a7662dd6
Dohou Daniel <dohoudaniel@example.com>
2022-09-09 18:13:36 -0700

    Printing numbers without using char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * function main - print numbers 0 to 9
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int f = 0;
+
+	for (; f < 10 ; f++)
+		putchar(')' + f);
+	putchar('\n');
+	return (0);
+}
