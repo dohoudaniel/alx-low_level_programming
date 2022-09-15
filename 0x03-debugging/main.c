85b403452fb9f0d3c500e16aad79cfd19c40993d
Dohou Daniel <dohoudaniel@example.com>
2022-09-15 16:39:46 +0100

    Task 0

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+  int i;
+  int a, b, c;
+
+  i = 98;
+  positive_or_negative(i);
+  largest_number(a, b, c);
+
+  return (0);
+}
