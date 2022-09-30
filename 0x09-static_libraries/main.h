454bd7c88cd96a8d0d11f06582dc0ab5796fb028
Dohou Daniel <dohoudaniel@example.com>
2022-10-01 00:22:08 +0100

    Still debugging

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,24 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-/**
- * File: main.h
- * Desc: Header file containing all prototypes for all functions 
- *       used in the 0x09-static_libraries directory
- */
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
-int _strlen(char s);
+int _strlen(char *s);
 void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
