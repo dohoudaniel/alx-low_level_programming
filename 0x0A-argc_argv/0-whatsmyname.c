c37c5e26be6a4a2fce71b9866bddca73c5c599a0
Dohou Daniel <dohoudaniel@example.com>
2022-10-01 22:02:37 +0100

    Betty!

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /*
- * main - Entry point of the program
+ * main - Prints the program name, folloewd by a new line
  *
  * Return: Always returns 0
  */
 
-int main(int __attribute__ ((__unused__)) argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 
