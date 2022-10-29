ef926c7b839324e6e82de3934dcb78870f680fb6
Dohou Daniel <dohoudanielfavour@gmail.com>
2022-10-29 19:06:31 +0100

    Added all missing main.c files to their repositories

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - Prints all the elements of a listint_t list
+ * @h: The listint_t list
+ *
+ * Return: The number of nodes in h
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes 
