f980c69436f0d120caee6c2ed8e3be5712007204
Dohou Daniel <dohoudanielfavour@gmail.com>
2022-11-13 22:50:43 +0100

    Debugged the issue

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 /**
- * add_nodeint_end - Adds a new node at the beginning
+ * add_nodeint - Adds a new node at the beginning
  *            of a listint_t list.
  * @head: A pointer to the address of the
  *	head of the listint_t list.
@@ -11,7 +11,7 @@
  * Return: If the function fails - NULL.
  * Otherwise - the address of the new element.
  */
-listint_t *add_nodeint_end(listint_t **head, const int n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *ptr;
 
