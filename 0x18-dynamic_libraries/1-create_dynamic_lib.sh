91cff032513f88fb548833a02cf54cecef246dbe
Dohou Daniel <dohoudanielfavour@gmail.com>
2023-05-03 02:10:51 +0100

    A Bash Script that creates a shared library liball.so

---
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -0,0 +1,10 @@
+# This is a Bash Script that creates a C dynamic or shared library.
+# Simply compile to create a dynamic library.
+
+#!/bin/bash
+echo -e "\e[1;32m Compiling .c files into object files ... \e[0m"
+gcc -fPIC -c *.c
+echo -e "\e[1;32m\nDone.\n\nCreating shared library liball.so ...\e[0m"
+gcc -shared *.o -o liball.so
+echo -e "\e[1;32m\nDone.\n\nInspecting and displaying list of symbols in liball.so ...\e[0m"
+nm -D liball.so
