cf0bc2e421125e037b53c17f11b6e9f66331ea49
Dohou Daniel <dohoudanielfavour@gmail.com>
2023-05-03 02:13:52 +0100

    A Bash Script that creates a shared library liball.so

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-echo -e "\e[1;32m Compiling .c files into object files ... \e[0m"
 gcc -fPIC -c *.c
-echo -e "\e[1;32m\nDone.\n\nCreating shared library liball.so ...\e[0m"
 gcc -shared *.o -o liball.so
-echo -e "\e[1;32m\nDone.\n\nInspecting and displaying list of symbols in liball.so ...\e[0m"
 nm -D liball.so
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
