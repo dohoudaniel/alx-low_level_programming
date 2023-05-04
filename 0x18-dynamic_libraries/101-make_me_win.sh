b2b4ee50feada1100b42081853aa21fba140f923
Dohou Daniel <dohoudanielfavour@gmail.com>
2023-05-04 21:51:19 +0100

    Advanced Task 2: Using LD_PRELOAD

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
-export LD_PRELOAD=/path/to/libwinning.so
+# wget -P /tmp/ https://github.com/dohoudaniel/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libwinning.so
+gcc -shared -fPIC -o libwinning.so winning_numbers.c
+export LD_PRELOAD=$PWD:libwinning.so
