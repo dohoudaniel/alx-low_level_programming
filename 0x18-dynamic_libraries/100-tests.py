5263babf1d4330a226101c8049b30ff58c6f2725
dohoudaniel <dohoudanielfavour@gmail.com>
2023-10-19 08:15:53 +0100

    Edit file.

--- 0x18-dynamic_libraries/100-tests.py
+++ 0x18-dynamic_libraries/100-tests.py
@@ -10,4 +10,3 @@ print("{} - {} = {}".format(a, b, cops.sub(a, b)))
 print("{} x {} = {}".format(a, b, cops.mul(a, b)))
 print("{} / {} = {}".format(a, b, cops.div(a, b)))
 print("{} % {} = {}".format(a, b, cops.mod(a, b)))
-julien@ubuntu:~/0x16. Doubly linked lists$ python3 100-tests.py
